6758bced4fcb98d9c4610b1a78070136b60835a7
Zak Ream <ream720@gmail.com>
2023-11-29 21:07:33 -0500

    added pagination

--- src/App.js
+++ src/App.js
@@ -10,21 +10,37 @@ const API_URL = "http://www.omdbapi.com?apikey=37199185";
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
 
   //function to fetch movies
-  const searchMovies = async (title) => {
+  const searchMovies = async (title, page = 1) => {
     //async arrow function e.g. async searchMovies(title) {}
-    const response = await fetch(`${API_URL}&s=${title}`); //async response API call
+    const response = await fetch(`${API_URL}&s=${title}&page=${page}`); //async response API call
     //data = movie data
     const data = await response.json();
+    console.log(data);
 
     setMovies(data.Search);
+    setTotalPages(Math.ceil(data.totalResults / 10));
   };
 
   //useEffect hook = action on component init/load, optional [] empty dependency array to call ONLY ONCE
   useEffect(() => {
-    searchMovies("The mummy");
+    searchMovies("");
   }, []);
+
+  const goToNextPage = () => {
+    setCurrentPage(currentPage + 1);
+    searchMovies(searchTerm, currentPage + 1);
+  };
+
+  const goToPreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+      searchMovies(searchTerm, currentPage - 1);
+    }
+  };
 
   return (
     <div className="app">
@@ -34,6 +50,11 @@ const App = () => {
           placeholder="search for movies"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              searchMovies(searchTerm);
+            }
+          }}
         />
         <img
           src={SearchIcon}
@@ -53,6 +74,14 @@ const App = () => {
           <h2>No movies found!</h2>
         </div>
       )}
+      <div className="pagination">
+        {currentPage > 1 && (
+          <button onClick={goToPreviousPage}>Previous</button>
+        )}
+        {currentPage < totalPages && (
+          <button onClick={goToNextPage}>Next</button>
+        )}
+      </div>
     </div>
   );
 };
